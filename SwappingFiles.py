86c891036af142a7dd587819814e5aaf4c0d7894
Besty <67360804+bestyajesh@users.noreply.github.com>
2021-06-09 16:03:05 +0530

    File Handling
    Swap, Backup- keep a copy, moving, and Organizing in the basis of extensions

---
+++ SwappingFiles.py
@@ -0,0 +1,8 @@
+def swapingFile():
+     fileName1 =  input("Enter the first file name:- ")
+     fileName2 =  input("Enter the second file name:- ")
+     file1 =  open(fileName1, 'r')
+     file2 =  open(fileName2, 'r')
+
+
+     
